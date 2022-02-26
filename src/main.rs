2a11279f9e8fb0a61ec7456049b0b6e5dd23c9b5
sxxrfx <sxxrfx@github.com>
2022-02-26 20:09:08 +0530

    Improving the Benchmark.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,25 @@ use hash_table_rs::benchmarks::*;
 use std::time::Instant;
 fn main() {
     const N: usize = 100_000;
-
+    let mut i = 10;
     let begin = Instant::now();
-    benchmark_the_crate_table(N);
+    loop {
+        if i == 0 {
+            break;
+        }
+        benchmark_the_crate_table(N);
+        i -= 1;
+    }
     println!("Crate HashTable :: {:>8.5}s", begin.elapsed().as_secs_f32());
 
     let begin = Instant::now();
-    benchmark_std_table(N);
+    i = 10;
+    loop {
+        if i == 0 {
+            break;
+        }
+        benchmark_std_table(N);
+        i -= 1;
+    }
     println!("'std' HashTable :: {:>8.5}s", begin.elapsed().as_secs_f32());
 }
